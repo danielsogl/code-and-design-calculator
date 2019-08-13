87f746d28aa344d5341a12c979152afd0865c6fd
Daniel Sogl <mytechde@outlook.com>
2019-08-13 16:50:51 +0200

    feat(app): c&p example code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,10 +3,46 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  value = '0';
+  oldValue = '0';
 
-  constructor() {}
+  lastOperator = 'x';
+  readyForNewInput = true;
+  numberGroups = [[7, 8, 9, 'x'], [4, 5, 6, '-'], [1, 2, 3, '+'], [0, 'c', '/', '=']];
 
+  onButtonPress(symbol: string) {
+    console.log(symbol);
+
+    if (+symbol) {
+      console.log('is a number');
+      if (this.readyForNewInput) {
+        this.value = '' + symbol;
+      } else {
+        this.value += '' + symbol;
+      }
+      this.readyForNewInput = false;
+    } else if (symbol === 'c') {
+      this.value = '0';
+      this.readyForNewInput = true;
+    } else if (symbol === '=') {
+      if (this.lastOperator === 'x') {
+        this.value = '' + parseInt(this.oldValue, 10) * parseInt(this.value, 10);
+      } else if (this.lastOperator === '-') {
+        this.value = '' + (parseInt(this.oldValue, 10) - parseInt(this.value, 10));
+      } else if (this.lastOperator === '+') {
+        this.value = '' + (parseInt(this.oldValue, 10) + parseInt(this.value, 10));
+      } else if (this.lastOperator === '/') {
+        this.value = '' + parseInt(this.oldValue, 10) / parseInt(this.value, 10);
+      }
+      this.readyForNewInput = true;
+    } else {
+      // operator
+      this.readyForNewInput = true;
+      this.oldValue = this.value;
+      this.lastOperator = symbol;
+    }
+  }
 }
